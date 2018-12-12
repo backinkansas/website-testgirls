0fa3a51d283dfe5255bfd0e68a4da02e27639a39
backinkansas <milena.martinsc@gmail.com>
2018-12-12 18:34:18 -0200

    meetup API integration for next event

---
+++ assets/js/meetupAPI.js
@@ -0,0 +1,59 @@
+const PT_MONTHS = {
+	0: 'JAN',
+	1: 'FEV',
+	2: 'MAR',
+	3: 'ABR',
+	4: 'MAI',
+	5: 'JUN',
+	6: 'JUL',
+	7: 'AGO',
+	8: 'SET',
+	9: 'OUT',
+	10: 'NOV',
+	11: 'DEZ'
+}
+  
+function handleDate(date) {
+	const day = date.getDate()
+	const month = PT_MONTHS[date.getMonth()]
+	return `${day} ${month}`
+}
+
+function handleDescription(text) {
+	return text.slice(3, 130)
+}
+  
+function createEventMarkup(result) {
+	const event = result.data[0]
+	console.log(event)
+	const originalDate = new Date(event.time)
+	const date = handleDate(originalDate)
+	const eventText = handleDescription(event.description)
+	const eventContainer = document.getElementsByClassName('upcoming-events__container')[0]
+	eventContainer.innerHTML = `
+	<div class="upcoming-events__card">
+		<h4 class="upcoming-events__card__date">${date}</h4>
+		<div class="upcoming-events__card__content">
+			<h5 class="upcoming-events__card__title">
+				<a href="${event.link}" target=_blank class="upcoming-events__card__link">${event.name}</a>
+			</h5>
+			<p class="upcoming-events__card__description">${eventText}...</p>
+		</div>
+	</div>`
+}
+
+function fetchEvents(url) {
+	$.ajax({
+		dataType:'jsonp',
+		method:'get',
+		url:url,
+		success:function(result) {
+			createEventMarkup(result)
+		}
+	});	
+}
+
+$(document).ready(function() {
+	fetchEvents("https://api.meetup.com/test-girls/events?photo-host=public&page=20&sig_id=252409091&sig=cee0a3f17064bf6e1c2993fe5a930e12ca159993&callback=?");	
+});
+
